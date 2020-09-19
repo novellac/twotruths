43f270921b87ebb1502f0b924e98a66a459e6ed6
Novella C <novella@altodev.net>
2020-09-19 10:22:07 -0400

    Make and use card component

---
+++ src/components/Card.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="$emit('calculate')">
+    {{ message }}
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
